86a85bfccde7d3f5e901672d87fc2192153c0e95
Denis622 <denisius94@gmail.com>
2017-05-28 19:23:03 +0300

    Непонятное

---
+++ Sandbox/src/main/java/ru/stqa/ptf/sandbox/Square.java
@@ -0,0 +1,18 @@
+package ru.stqa.ptf.sandbox;
+
+/**
+ * Created by Denis on 28.05.2017.
+ */
+public class Square {
+
+    public double l;
+
+    public Square(double l) {
+        this.l = l;
+    }
+
+    public double area() {
+        return this.l * this.l;
+    }
+
+}
