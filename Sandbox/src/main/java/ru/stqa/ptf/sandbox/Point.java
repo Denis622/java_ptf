361c305834c5838413ad53f8f0cc15af3d320a14
Denis622 <denisius94@gmail.com>
2017-06-04 23:07:06 +0300

    Получились точки

--- Sandbox/src/main/java/ru/stqa/ptf/sandbox/Point.java
+++ Sandbox/src/main/java/ru/stqa/ptf/sandbox/Point.java
@@ -1,24 +1,23 @@
 package ru.stqa.ptf.sandbox;
 
 /**
- * Created by Denis on 28.05.2017.
+ * Created by Denis on 04.06.2017.
  */
 public class Point {
-    public double x;
-    public double y;
 
-    double getDistance(Point a) {
-        return Math.sqrt( Math.pow(x-a.x, 2) + Math.pow(y-a.y, 2) );
+   public double x;
+   public double y;
 
-    public Point(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
+   public Point(double x, double y) {
+       this.x = x;
+       this.y = y;
+   }
 
 public double distance(Point p2) {
-        double x = this.x - p2.x;
-        double y = this.y - p2.y;
-        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+       double x = this.x - p2.x;
+       double y = this.y - p2.y;
+return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+
+   }
 }
 
-}
--- Sandbox/src/main/java/ru/stqa/ptf/sandbox/Starter.java
+++ Sandbox/src/main/java/ru/stqa/ptf/sandbox/Starter.java
@@ -1,7 +1,15 @@
 package ru.stqa.ptf.sandbox;
 
 /**
- * Created by Denis on 28.05.2017.
+ * Created by Denis on 04.06.2017.
  */
 public class Starter {
+
+    public static void main(String[] args) {
+
+        Point p1 = new Point(5, 5);
+        Point p2 = new Point(10, 10);
+
+        System.out.println("Расстояние между точками P1" + "(" + p1.x + "," + p1.y + ")" + " и " + "P2(" + p2.x + "," + p2.y + ")" + "=" + p2.distance(p1));
+    }
 }
