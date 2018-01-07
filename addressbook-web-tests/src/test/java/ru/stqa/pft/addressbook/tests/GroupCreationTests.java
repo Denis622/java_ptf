3499fded0128c4fde7f4a922ae594c53e16d723a
Denis622 <denisius94@gmail.com>
2018-01-07 23:09:57 +0300

    Added asserts of counts for tests with groups
    Added asserts of counts for group creation, modification and deletion tests.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,4 +1,5 @@
 package ru.stqa.pft.addressbook.tests;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.models.GroupData;
 
@@ -7,6 +8,9 @@ public class GroupCreationTests extends TestBase {
     @Test
     public void testGroupCreation() {
         app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();
         app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before + 1);
     }
 }
