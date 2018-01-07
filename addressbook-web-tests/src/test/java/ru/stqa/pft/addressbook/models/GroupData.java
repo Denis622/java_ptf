401b9032330018bb5292a769f45cefd89aac28de
Denis622 <denisius94@gmail.com>
2018-01-08 01:03:56 +0300

    Added asserts of whole lists

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/models/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/models/GroupData.java
@@ -5,6 +5,28 @@ public class GroupData {
     private final String footer;
     private final String header;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GroupData groupData = (GroupData) o;
+
+        return name != null ? name.equals(groupData.name) : groupData.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
     public GroupData(String name, String footer, String header) {
         this.name = name;
         this.footer = footer;
