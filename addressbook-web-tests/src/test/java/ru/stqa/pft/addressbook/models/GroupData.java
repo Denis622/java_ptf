58ee389d8a2dc0ce76bfec3b08c2f041adbbcb89
Denis622 <denisius94@gmail.com>
2018-01-21 21:39:23 +0300

    Unordered Collections of Objects
    Used unordered Collections of Objects for group modification test.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/models/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/models/GroupData.java
@@ -1,9 +1,36 @@
 package ru.stqa.pft.addressbook.models;
 
 public class GroupData {
+    private final String id;
     private final String name;
     private final String footer;
     private final String header;
+
+    public GroupData(String name, String footer, String header) {
+        this.id = null;
+        this.name = name;
+        this.footer = footer;
+        this.header = header;
+    }
+
+    public GroupData(String id, String name, String footer, String header) {
+        this.id = id;
+        this.name = name;
+        this.footer = footer;
+        this.header = header;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -12,25 +39,15 @@ public class GroupData {
 
         GroupData groupData = (GroupData) o;
 
+        if (id != null ? !id.equals(groupData.id) : groupData.id != null) return false;
         return name != null ? name.equals(groupData.name) : groupData.name == null;
     }
 
     @Override
     public int hashCode() {
-        return name != null ? name.hashCode() : 0;
-    }
-
-    @Override
-    public String toString() {
-        return "GroupData{" +
-                "name='" + name + '\'' +
-                '}';
-    }
-
-    public GroupData(String name, String footer, String header) {
-        this.name = name;
-        this.footer = footer;
-        this.header = header;
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
     }
 
     public String getName() {
